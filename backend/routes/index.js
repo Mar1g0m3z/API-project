2bec61f165077612768fd8aa8970ef0217664dcb
Marisela <mariselaalejandragomez@gmail.com>
2023-12-20 15:26:38 -0800

    added API router

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const apiRouter = require("./api");
 // setting a cookie on the response with the name XSRF-Token to the
 // value of req.csrfToken
 
@@ -12,5 +12,7 @@ router.get("/api/csrf/restore", (req, res) => {
 		"XSRF-Token": csrfToken,
 	});
 });
+
+router.use("/api", apiRouter);
 
 module.exports = router;
