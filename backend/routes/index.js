13d6966624b4e74bc3df9cebb5fb3c3d6cadb073
Marisela <mariselaalejandragomez@gmail.com>
2023-12-20 14:03:00 -0800

    initialized express and sequelize with CSRF protection

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/hello/world", function (req, res) {
-	res.cookie("XSRF-TOKEN", req.csrfToken());
-	res.send("Hello World!");
+// setting a cookie on the response with the name XSRF-Token to the
+// value of req.csrfToken
+
+// Add a XSRF-TOKEN cookie
+router.get("/api/csrf/restore", (req, res) => {
+	const csrfToken = req.csrfToken();
+	res.cookie("XSRF-TOKEN", csrfToken);
+	res.status(200).json({
+		"XSRF-Token": csrfToken,
+	});
 });
 
 module.exports = router;
