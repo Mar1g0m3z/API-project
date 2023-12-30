f10fdffb4e611d4f3e321e7e701ce97a9fb9150b
Marisela <mariselaalejandragomez@gmail.com>
2023-12-30 01:28:49 -0800

    i dont even know what happened, but i fixed it...i think

--- backend/db/models/review.js
+++ backend/db/models/review.js
@@ -8,8 +8,16 @@ module.exports = (sequelize, DataTypes) => {
 		 * The `models/index` file will call this method automatically.
 		 */
 		static associate(models) {
-			Review.belongsTo(models.Spot, { foreignKey: "spotId" });
-			Review.belongsTo(models.User, { foreignKey: "userId" });
+			Review.belongsTo(models.Spot, {
+				foreignKey: "spotId",
+				onDelete: "CASCADE",
+				hooks: true,
+			});
+			Review.belongsTo(models.User, {
+				foreignKey: "userId",
+				onDelete: "CASCADE",
+				hooks: true,
+			});
 			Review.hasMany(models.ReviewImage, {
 				foreignKey: "reviewId",
 				onDelete: "CASCADE",
