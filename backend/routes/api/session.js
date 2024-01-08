03972ea79b1e6633c5194d175b8fefedbae2537b
Marisela <mariselaalejandragomez@gmail.com>
2024-01-07 22:03:36 -0800

    change credential error to message error

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -55,7 +55,7 @@ router.post("/", validateLogin, async (req, res, next) => {
 		const err = new Error("Login failed");
 		err.status = 401;
 		err.title = "Login failed";
-		err.errors = { credential: "The provided credentials were invalid." };
+		err.errors = { message: "The provided credentials were invalid." };
 		return next(err);
 	}
 
